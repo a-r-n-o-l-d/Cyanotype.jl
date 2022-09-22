de74ac9e368163b9be22ead09a4fa61c36fbeb6d
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-09-22 19:24:48 +0200

    Setting up Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,11 @@ using Cyanotype
 #using Flux
 using Test
 using Aqua
-using JET
-
-#Aqua.test_all(Cyanotype)
-
 
 @testset verbose = true "Cyanotype.jl" begin
     @testset verbose = true "Code quality" begin
         @testset verbose = true "Aqua" begin
-            #Aqua.test_all(Cyanotype) => ambiguities from
+            #Aqua.test_all(Cyanotype) => ambiguities from Flux, Zygote, StatsBase
             Aqua.test_ambiguities(Cyanotype)
             Aqua.test_unbound_args(Cyanotype)
             Aqua.test_undefined_exports(Cyanotype)
@@ -18,10 +14,6 @@ using JET
             Aqua.test_stale_deps(Cyanotype)
             Aqua.test_deps_compat(Cyanotype)
             Aqua.test_project_toml_formatting(Cyanotype)
-        end
-        @testset verbose = true "JET" begin
-            jet_report = JET.report_package(Cyanotype)
-            #@test string(jet_report) == "No errors detected\n"
         end
     end
 
