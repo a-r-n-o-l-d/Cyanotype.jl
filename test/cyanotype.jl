4454212d86aeebac12a9c90aff205008ef8bb6c6
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-11-07 18:49:59 +0100

    tests

--- test/cyanotype.jl
+++ test/cyanotype.jl
@@ -1,10 +1,59 @@
-module CynotypeTest
+#module CynotypeTest
 
 using Cyanotype
 using Flux
 using Cyanotype: KwargsMapping, @cyanotype
 #import Cyanotype: mapping
 
+@cyanotype (
+"""
+Bla-bla
+"""
+) (
+struct Foo
+    a::Int = 1
+    b::Int = 2
+    c = 3
+    d
+end
+)
+
+# Checks the documentation generation
+@cyanotype (
+"""
+Bla-bla
+"""
+) (
+struct Foo1
+    """Activation function."""
+    activation
+end
+)
+@test !isempty(eval(macroexpand(@__MODULE__, :(@doc $Foo1))))
+
+# Checks the correctness of parametric type declaration
+@cyanotype (
+"""
+Bla-bla
+"""
+) (
+struct Foo2{F <: Function}
+    activation::F = relu
+end
+)
+@test Foo2().activation isa Function
+
+# Checks the default inheritance from AbstractBlueprint
+@cyanotype (
+"""
+Bla-bla
+"""
+) (
+struct Foo3
+
+end
+)
+@test Foo3() isa Cyanotype.AbstractBlueprint
 
 @cyanotype (
 """
@@ -23,11 +72,30 @@ KwargsMapping(;
     def_values  = (Flux.zeros32, Flux.ones32, true, true, 1f-5, 0.1f0)
     )
 ) (
-struct BatchNormTest1{N <: CyFloat} <: Cyanotype.AbstractBlueprint
+struct BatchNormTest{N <: CyFloat}
     """activation function for BatchNorm layer"""
     activation
 end
 )
+
+# Checks the correctness of fields declaration
+for f in (:init_bias, :init_scale, :affine, :track_stats, :epsilon, :momentum)
+    @test hasfield(BatchNormTest, f)
+end
+
+# Checks cyanotype function
+bn = BatchNormTest(; activation = relu)
+bn = cyanotype(bn; affine = false, epsilon = 0f0)
+@test bn.affine == false
+@test bn.epsilon == 0f0
+
+# Checks kwargs function, for now calling of the curate function
+bn = BatchNormTest(; activation = relu)
+@test haskey(Cyanotype.kwargs(bn), :initβ)
+@test haskey(Cyanotype.kwargs(bn), :initγ)
+@test haskey(Cyanotype.kwargs(bn), :ϵ)
+@test !haskey(Cyanotype.kwargs(bn), :activation)
+
 
 #methods(BatchNormTest1) |> println
 
@@ -37,4 +105,4 @@ end
 #cya |> println
 
 #cyanotype(cya; affine = false) |> println
-end
+#end
