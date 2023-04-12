dca9114856e5930e56b397bac55aa848e7c6add2
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2023-04-12 17:52:45 +0200

    cleaning

--- src/uchain.jl
+++ src/uchain.jl
@@ -1,12 +1,10 @@
 """
     uchain(;encoders, decoders, bridge, connection)
 
-Build a `Chain` with U-Net like architecture. `encoders` and `decoders` are
-vectors of encoding/decoding blocks, from top to bottom (see diagram below).
-`bridge` is the bottom part of U-Net  architecture.
-Each level of the U-Net is connected through a 2-argument callable `connection`.
-`connection` could be a vector in (unusual) case the way levels are connected
-vary from one level to another.
+Build a `Chain` with U-Net like architecture. `encoders` and `decoders` are vectors of
+encoding/decoding blocks, from top to bottom (see diagram below). `bridge` is the bottom
+part of U-Net  architecture. Each level of the U-Net is connected through a channel
+concatenation (◊ symbol in the diagram below).
 
 Notes :
 - usually encoder block starts with a 'MaxPool' to downsample image by 2, except
@@ -15,29 +13,27 @@ the first level encoder.
 except the first level decoder.
 
 ```
-+---------+                                                          +---------+
-|encoder 1|                                                          |decoder 1|
-+---------+                                                          +---------+
-     |------------------------------------------------------------------->^
-     |                                                                    |
-     |   +---------+                                         +---------+  |
-     +-->|encoder 2|                                         |decoder 2|--+
-         +---------+                                         +---------+
-              |-------------------------------------------------->^
-              |                                                   |
-              |   +---------+                        +---------+  |
-              +-->|encoder 3|                        |decoder 3|--+
-                  +---------+                        +---------+
-                       |--------------------------------->^
-                       |                                  |
-                       |   +---------+       +---------+  |
-                       +-->|encoder 4|       |decoder 4|--+
-                           +---------+       +---------+
-                                |---------------->^
-                                |                 |
-                                |    +--------+   |
-                                +--->| bridge |---+
-                                     +--------+
+┌─────────┐                                                          ┌─────────┐
+│Encoder 1│                                                          │Decoder 1│
+└────┬────┘                                                          └─────────┘
+     │                                                                    ▴
+     │                                                                    │
+     ├───────────────────────────────────────────────────────────────────▸◊
+     │    ┌─────────┐                                     ┌─────────┐     │
+     └───▸│Encoder 2│                                     │Decoder 2├─────┘
+          └────┬────┘                                     └─────────┘
+               │                                               ▴
+               │                                               │
+               ├──────────────────────────────────────────────▸◊
+               │     ┌─────────┐               ┌─────────┐     │
+               └────▸│Encoder 3│               │Decoder 3├─────┘
+                     └────┬────┘               └─────────┘
+                          │                         ▴
+                          │                         │
+                          ├────────────────────────▸◊
+                          │       ┌─────────┐       │
+                          └──────▸│ Bridge  ├───────┘
+                                  └─────────┘
 ```
 See also [`chcat`](@ref).
 """
@@ -46,29 +42,33 @@ function uchain(; encoders, decoders, bridge)
     The number of encoders should be equal to the number of decoders.
     """)
     # build from bottom to top
-    l = ubridge(bridge)
+    l = _ubridge(bridge)
     for (e, d) in zip(reverse(encoders[2:end]), reverse(decoders[2:end]))
-        l = uconnect(e, l, d)
-        l = SkipConnection(l, chcat) # Parallel(c, l, cross_connector_block)
+        l = _uconnect(e, l, d)
+        l = SkipConnection(l, chcat) # Parallel(chcat, l, cross_connector_block)
     end
-    uconnect(encoders[1], l, decoders[1])
+    _uconnect(encoders[1], l, decoders[1])
 end
 
-@inline uconnect(enc, prl, dec) = Chain(enc, prl, dec)
+############################################################################################
+#                                   INTERNAL FUNCTIONS                                     #
+############################################################################################
+
+@inline _uconnect(enc, prl, dec) = Chain(enc, prl, dec)
 
 for T1 in [:Chain :AbstractArray], T2 in [:Chain :AbstractArray]
     @eval begin
-        @inline uconnect(enc::($T1), prl, dec::($T2)) = Chain(enc..., prl, dec...)
+        @inline _uconnect(enc::($T1), prl, dec::($T2)) = Chain(enc..., prl, dec...)
     end
 end
 
 for T in [:Chain :AbstractArray]
     @eval begin
-        @inline uconnect(enc::($T), prl, dec) = Chain(enc..., prl, dec)
-        @inline uconnect(enc, prl, dec::($T)) = Chain(enc, prl, dec...)
+        @inline _uconnect(enc::($T), prl, dec) = Chain(enc..., prl, dec)
+        @inline _uconnect(enc, prl, dec::($T)) = Chain(enc, prl, dec...)
     end
 end
 
-@inline ubridge(b) = SkipConnection(b, chcat)
+@inline _ubridge(b) = SkipConnection(b, chcat)
 
-@inline ubridge(b::AbstractArray) = SkipConnection(Chain(b...), chcat)
+@inline _ubridge(b::AbstractArray) = SkipConnection(Chain(b...), chcat)
