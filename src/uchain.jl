d7aa546638a30a61088ccf95e62cb7d910fd9878
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2023-04-12 16:58:06 +0200

    uchain

---
+++ src/uchain.jl
@@ -0,0 +1,84 @@
+"""
+    uchain(;encoders, decoders, bridge, connection)
+
+Build a `Chain` with U-Net like architecture. `encoders` and `decoders` are
+vectors of encoding/decoding blocks, from top to bottom (see diagram below).
+`bridge` is the bottom part of U-Net  architecture.
+Each level of the U-Net is connected through a 2-argument callable `connection`.
+`connection` could be a vector in (unusual) case the way levels are connected
+vary from one level to another.
+
+Notes :
+- usually encoder block starts with a 'MaxPool' to downsample image by 2, except
+the first level encoder.
+- usually decoder block ends with a 'ConvTranspose' to upsample image by 2,
+except the first level decoder.
+
+```
++---------+                                                          +---------+
+|encoder 1|                                                          |decoder 1|
++---------+                                                          +---------+
+     |------------------------------------------------------------------->^
+     |                                                                    |
+     |   +---------+                                         +---------+  |
+     +-->|encoder 2|                                         |decoder 2|--+
+         +---------+                                         +---------+
+              |-------------------------------------------------->^
+              |                                                   |
+              |   +---------+                        +---------+  |
+              +-->|encoder 3|                        |decoder 3|--+
+                  +---------+                        +---------+
+                       |--------------------------------->^
+                       |                                  |
+                       |   +---------+       +---------+  |
+                       +-->|encoder 4|       |decoder 4|--+
+                           +---------+       +---------+
+                                |---------------->^
+                                |                 |
+                                |    +--------+   |
+                                +--->| bridge |---+
+                                     +--------+
+```
+See also [`chcat`](@ref).
+"""
+function uchain(;encoders, decoders, bridge, connection)
+    length(encoders) == length(decoders) || throw(ArgumentError(
+        "The number of encoders should be equal to the number of decoders."))
+
+    if isa(connection, AbstractArray)
+        length(encoders) == length(connection) || throw(ArgumentError(
+            "The number of connections should be equal to the number of encoders/decoders."))
+    else
+        connection = repeat([connection], length(encoders))
+    end
+
+    # build from bottom to top
+    ite = zip(reverse(encoders[2:end]),
+              reverse(decoders[2:end]),
+              reverse(connection[1:(end - 1)]))
+    l = ubridge(bridge, connection[end])
+    for (e, d, c) ∈ ite
+        l = uconnect(e, l, d)
+        l = SkipConnection(l, c) # Parallel(c, l, cross_connector_block)
+    end
+    uconnect(encoders[1], l, decoders[1]) # Chain(input = input, backbone = uconnect(encoders[1], l, decoders[1]), classifier = classifier)
+end
+
+@inline uconnect(enc, prl, dec) = Chain(enc, prl, dec)
+
+for T1 ∈ [:Chain :AbstractArray], T2 ∈ [:Chain :AbstractArray] # AbstractVector
+    @eval begin
+        @inline uconnect(enc::($T1), prl, dec::($T2)) = Chain(enc..., prl, dec...)
+    end
+end
+
+for T ∈ [:Chain :AbstractArray]
+    @eval begin
+        @inline uconnect(enc::($T), prl, dec) = Chain(enc..., prl, dec)
+        @inline uconnect(enc, prl, dec::($T)) = Chain(enc, prl, dec...)
+    end
+end
+
+@inline ubridge(b, c) = SkipConnection(b, c) #_ubridge
+
+@inline ubridge(b::AbstractArray, c) = SkipConnection(Chain(b...), c)
