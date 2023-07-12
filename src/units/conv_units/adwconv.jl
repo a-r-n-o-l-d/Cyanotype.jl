91d41832d436d4d35c670c25093111ef4000fa12
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2023-07-12 14:14:25 +0200

    implemtentation of AxialDWConvBp

--- src/units/conv_units/adwconv.jl
+++ src/units/conv_units/adwconv.jl
@@ -1,17 +1,18 @@
-@cyanotype constructor=false begin
+@cyanotype begin #constructor=false
     """
     https://arxiv.org/pdf/2306.16103v2.pdf
     """
-    struct AxialDWConvBp{A<:Function,P<:Union{SamePad,Int},I<:Function} <: AbstractConvBp
+    struct AxialDWConvBp{A<:Function,N<:Union{Nothing,AbstractNormBp},P<:Union{SamePad,Int},I<:Function} <: AbstractConvBp
         @volume
         @activation(identity)
-        stride::Int
-        pad::P
-        dilation::Int
-        init::I
+        stride::Int = 1
+        pad::P = SamePad()
+        dilation::Int = 1
+        init::I = glorot_uniform
+        normalization::N = BatchNormBp()
     end
 end
-
+#=
 AxialDWConvBp(; volume=false, activation=identity, stride=1, pad=SamePad(), dilation=1, init=glorot_uniform) = AxialDWConvBp(
     volume,
     activation,
@@ -20,12 +21,13 @@ AxialDWConvBp(; volume=false, activation=identity, stride=1, pad=SamePad(), dila
     dilation,
     init
 )
-
+=#
 make(bp::AxialDWConvBp, ksize, channels::Int) = make(bp, ksize, channels => channels)
 
 function make(bp::AxialDWConvBp, ksize, channels::Pair)
     in_chs, out_chs = channels
-    conv(k) = Conv(k, in_chs => in_chs, stride=bp.stride, pad=bp.pad, dilation=bp.dilation, init=bp.init, bias=false)
+    conv(k) = DepthwiseConv(k, in_chs => in_chs, stride=bp.stride, pad=bp.pad, dilation=bp.dilation, init=bp.init,
+                            bias=bp.normalization isa Nothing)
     layers = []
     if bp.volume
         push!(layers, conv((ksize, 1, 1)))
@@ -35,6 +37,13 @@ function make(bp::AxialDWConvBp, ksize, channels::Pair)
         push!(layers, conv((ksize, 1)))
         push!(layers, conv((1, ksize)))
     end
+    norm = bp.normalization isa Nothing ? identity : BatchNorm(in_chs)
     pwc = PointwiseConvBp(activation=bp.activation, pad=bp.pad, init=bp.init)
-    [Parallel(+, layers...), BatchNorm(in_chs), make(pwc, in_chs => out_chs)]
+    flatten_layers(
+        [
+            Parallel(+, layers...),
+            norm,
+            make(pwc, in_chs => out_chs)
+        ]
+    )
 end
