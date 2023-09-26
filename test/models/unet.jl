d9c2df15da8711343aa43e034658da43d84df8d4
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2023-09-26 19:02:09 +0200

    UNet2Bp: a level parametric UNetBp

--- test/models/unet.jl
+++ test/models/unet.jl
@@ -9,3 +9,13 @@ top = PixelClassifierBp(nclasses=4)
 unet = UNetBp(encoder=enc, decoder=dec, bridge=bdg, top=top, expansion=2, basewidth=16)
 m = make(unet)
 @test Flux.outputsize(m, (32, 32, 3, 2)) == (32, 32, 4, 2)
+
+kfunc(level) = if level == 1
+    7
+else
+    3
+end
+
+unet = UNet2Bp(encoder=enc, decoder=dec, bridge=bdg, top=top, expansion=2, ksize=kfunc, basewidth=16)
+m = make(unet)
+@test Flux.outputsize(m, (32, 32, 3, 2)) == (32, 32, 4, 2)
